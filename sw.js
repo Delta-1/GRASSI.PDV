52e4ee500a4ac57fa5119632ed92cd8165be35c3
Delts <63209577+Delta-1@users.noreply.github.com>
2026-08-15 18:36:43 -0400

    Aprimora visual arredondado e interações em sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grassi-shell-v5';
+const CACHE = 'grassi-shell-v6';
 const CORE = ['./','./index.html','./pdv.html','./styles.css','./config.js','./backend.js','./app.js','./manifest.webmanifest','./assets/icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
