883f105701bd7ac39fd9e57bf6ebe143fc48af7c
Delts <63209577+Delta-1@users.noreply.github.com>
2026-08-15 23:45:23 -0400

    chore: renovar cache offline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grassi-shell-v11';
+const CACHE = 'grassi-shell-v12';
 const CORE = ['./','./index.html','./pdv.html','./styles.css','./config.js','./backend.js','./app.js','./manifest.webmanifest','./assets/icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
